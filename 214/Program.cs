97a9e1d48202170c14a7f6beee9154da38fcd801
LTFYR <lutfiyariea@code.edu.az>
2022-02-10 08:58:43 +0400

    Add project files.

---
+++ 214/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _214
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2,1,4 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
